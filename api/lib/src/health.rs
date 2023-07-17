277b75b357c875dbadc92754d15d049ac7781a55
mwombeki <hackerlubex@protonmail.com>
2023-07-17 12:02:19 +0300

    add health check endpoint

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,21 +1,8 @@
-use actix_web::get;
+use actix_web::{get, HttpResponse};
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
-
-#[tracing::instrument]
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-
-    match result {
-        Ok(version) => version,
-        Err(e) => format!("Error: {:?}", e),
-    }
+#[get("health")]
+async fn health() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("version", "0.0.1"))
+        .finish()
 }
